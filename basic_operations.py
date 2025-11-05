74e5c7dc9d9c800732601f1cdbf9e9834dd6991a
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-05 06:38:47 +0100

    add basic arithmetic

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
