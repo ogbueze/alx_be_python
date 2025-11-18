60da8a437517264083c8fbba8c7e9bf403acc71c
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-18 16:52:53 +0100

    add datetime calculator

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+def display_current_datetime(): 
+    current_date = datetime.datetime.today()
+    print(f"Current date and time: {current_date}")
+    future_date = input('Enter the number of days to add to the current date: ')
+    if type(int(future_date)) == int:
+        calculated_date = datetime.timedelta(days=int(future_date))
+        print(calculated_date + current_date)
+
+display_current_datetime()
