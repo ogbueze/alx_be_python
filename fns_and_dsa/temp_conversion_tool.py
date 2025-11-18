79ef4a27f15b4de7419fb95210c18cbe986b91a2
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-18 18:06:47 +0100

    debug

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,11 +2,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    celcius = (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    celcius = (fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR ) - 32
     return celcius
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
 
