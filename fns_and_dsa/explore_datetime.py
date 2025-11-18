14a0ed94d898c16e2f6e47a18374b8d0ca3a2627
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-18 16:56:34 +0100

    debug

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 
 def display_current_datetime(): 
-    current_date = datetime.today
+    current_date = datetime.now()
     print(f"Current date and time: {current_date}")
     future_date = input('Enter the number of days to add to the current date: ')
     if type(int(future_date)) == int:
